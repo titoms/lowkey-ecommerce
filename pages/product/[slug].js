93ebf9c1caf885e7b680258992e52682be8df89f
C Crognier <christophe.crognier@gmail.com>
2023-04-24 23:41:26 +0200

    Cart context and products counting

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,30 @@
 import Layout from '@/components/Layout';
+import { Store } from '@/utils/Store';
 import data from '@/utils/data';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Product NOT Found</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert('Sorry, Product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
 
   return (
     <Layout title={product.name}>
@@ -57,7 +70,11 @@ export default function ProductScreen() {
                 ${product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
               </div>
             </div>
-            <button className="primary-button w-full" type="button">
+            <button
+              className="primary-button w-full"
+              type="button"
+              onClick={addToCartHandler}
+            >
               Add to cart
             </button>
           </div>
