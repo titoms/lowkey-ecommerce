2b236f7dd03ba97b0a0c6633e9f22366a2ad9713
C Crognier <christophe.crognier@gmail.com>
2023-04-25 11:55:15 +0200

    Cart view and checkout button

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,9 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
+import React, { useContext } from 'react';
 import Link from 'next/link';
+import { Store } from '@/utils/Store';
 
 export default function ProductItem({ product }) {
+  const { state, dispatch } = useContext(Store);
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    if (product.countInStock < quantity) {
+      alert('Sorry, Product is out of stock');
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
+
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
@@ -19,7 +33,11 @@ export default function ProductItem({ product }) {
         </Link>
         <p className="mb-2">{product.brand}</p>
         <p>$ {product.price}</p>
-        <button className="primary-button" type="button">
+        <button
+          className="primary-button"
+          type="button"
+          onClick={addToCartHandler}
+        >
           Add to cart
         </button>
       </div>
---
+++ pages/cart.js
@@ -0,0 +1,91 @@
+import Layout from '@/components/Layout';
+import { Store } from '@/utils/Store';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useContext } from 'react';
+import { XCircleIcon } from '@heroicons/react/24/outline';
+import { useRouter } from 'next/router';
+
+export default function CartScreen() {
+  const { state, dispatch } = useContext(Store);
+  const router = useRouter();
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const removeItemFromCartHandler = (item) => {
+    dispatch({ type: 'CART_REMOVE_ITEM', payload: item });
+  };
+
+  return (
+    <Layout title="Shopping Cart">
+      <h1 className="mb-4 text-xl">Shopping Cart</h1>
+      {cartItems.length === 0 ? (
+        <div>
+          Cart is empty. <Link href="/">Go shopping</Link>
+        </div>
+      ) : (
+        <div className="grid mid:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <table className="min-w-full">
+              <thead className="border-b">
+                <tr>
+                  <th className="px-5 text-left">Item</th>
+                  <th className="p-5 text-right">Quantity</th>
+                  <th className="p-5 text-right">Price</th>
+                  <th className="p-5">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.slug} className="border-b">
+                    <td>
+                      <Link
+                        className="flex items-center"
+                        href={`/product/${item.slug}`}
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        ></Image>
+                        &nbsp;
+                        {item.name}
+                      </Link>
+                    </td>
+                    <td className="p-5 text-right">{item.quantity}</td>
+                    <td className="p-5 text-right">${item.price}</td>
+                    <td className="p-5 text-center">
+                      <button onClick={() => removeItemFromCartHandler(item)}>
+                        <XCircleIcon className="h-5 w-5"></XCircleIcon>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="card p-5">
+            <ul>
+              <li>
+                <div className="pb-3 text-xl">
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) : $
+                  {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                </div>
+              </li>
+              <li className="">
+                <button
+                  onClick={() => router.push('/shipping')}
+                  className="primary-button w-full"
+                >
+                  Checkout
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+}
