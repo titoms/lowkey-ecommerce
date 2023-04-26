b9ee0e2612c1334e33c4c550c247174ec2c99b71
C Crognier <christophe.crognier@gmail.com>
2023-04-26 22:51:18 +0200

    First try of API for back end

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,3 +1,5 @@
+import Product from '@/models/Product';
+
 const { default: User } = require('@/models/User');
 const { default: data } = require('@/utils/data');
 const { default: db } = require('@/utils/db');
@@ -6,6 +8,8 @@ const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'Seeded successfully' });
 };
